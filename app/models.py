a7db42af549d89a843aa6c419f311285ce69319c
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-19 09:02:20 +0300

    password properties and validation

--- app/models.py
+++ app/models.py
@@ -26,34 +26,34 @@ class User(UserMixin, db.Model):
 
 
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
-#
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
-#
-#     def check_password(self,password):
-#         return check_password_hash(self.password_hash,password)
-#         '''
-#     with these two methods in place, a user object is now
-#     able to do secure password verification
-#     '''
-#     def avatar(self, size):
-#         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-#         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
-#     '''
-#     The new avatar() method of the User class returns the URL of the user's avatar image,
-#     scaled to the requested size in pixels.For users that don't have an avatar registered, an "identicon" image will be generated
-#     The verify_reset_password_token() is a static method, which means that it can be invoked directly from the class
-#     '''
-#     def __repr__(self):
-#         return '{}'.format(self.username)
-#     '''
-#     Flask-login keeps track of the logged in
-#     user by storing its unique identifier in Flask's
-#     user session.
-#     '''
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+        '''
+    with these two methods in place, a user object is now
+    able to do secure password verification
+    '''
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    '''
+    The new avatar() method of the User class returns the URL of the user's avatar image,
+    scaled to the requested size in pixels.For users that don't have an avatar registered, an "identicon" image will be generated
+    The verify_reset_password_token() is a static method, which means that it can be invoked directly from the class
+    '''
+    def __repr__(self):
+        return '{}'.format(self.username)
+    '''
+    Flask-login keeps track of the logged in
+    user by storing its unique identifier in Flask's
+    user session.
+    '''
 #     @login_manager.user_loader
 #     def load_user(id):
 #         return User.query.get(int(id))
