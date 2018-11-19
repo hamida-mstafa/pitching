8d87f9764a31902578e7ed7bc7ac50f1aecfd3ff
hamida-mstafa <hamidamstafa@gmail.com>
2018-11-19 09:41:38 +0300

    added unitest to the ,anage.py

---
+++ manage.py
@@ -0,0 +1,27 @@
+from app import create_app,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Pitch,Comments
+app = create_app('development')
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+'''
+adds the databse instance and models to the shell session
+'''
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch =Pitch,Comments=Comments )
+
+if __name__ == '__main__':
+    manager.run()
